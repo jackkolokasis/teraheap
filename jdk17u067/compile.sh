f02578fc3a4fbea7313c3d50143eafb48970dbe2
Jack Kolokasis <kolokasis@ics.forth.gr>
2022-12-20 17:32:34 +0200

    [feat] Initial commit jdk17u-0.6.7

---
+++ jdk17u067/compile.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+
+###################################################
+#
+# file: compile.sh
+#
+# @Author:   Iacovos G. Kolokasis
+# @Version:  07-03-2021 
+# @email:    kolokasis@ics.forth.gr
+#
+# Compile JVM
+#
+###################################################
+
+# Define some variables for pretty printing
+ESC='\033[' 
+
+# Attributes
+NORMAL=0
+BOLD=1
+
+# Foreground colors
+RED_FG=31
+GREEN_FG=32
+YELLOW_FG=33
+
+# Presets
+BRED=${ESC}${BOLD}';'${RED_FG}'m'
+BGREEN=${ESC}${BOLD}';'${GREEN_FG}'m'
+BYELLOW=${ESC}${BOLD}';'${YELLOW_FG}'m'
+RESET=${ESC}${NORMAL}'m'
+
+function usage()
+{
+    echo
+    echo "Usage:"
+    echo -n "      $0 [option ...] [-h]"
+    echo
+    echo "Options:"
+    echo "      -r  Build release without debug symbols"
+    echo "      -d  Build with debug symbols"
+    echo "      -c  Clean and make"
+    echo "      -u  Update JVM in root directory"
+    echo "      -h  Show usage"
+    echo
+
+    exit 1
+}
+  
+# Compile without debug symbols
+function release() 
+{
+  make dist-clean
+  bash ./configure \
+    --with-jobs=32 \
+    --with-extra-cflags='-O3' \
+    --with-extra-cxxflags='-O3' \
+    --with-target-bits=64 \
+    --with-extra-ldflags=-lregions
+  
+  intercept-build make
+  cd ../ 
+  compdb -p jdk17u067 list > compile_commands.json
+  mv compile_commands.json jdk17u067
+  cd - || exit
+}
+
+# Compile with debug symbols and assertions
+function debug_symbols_on() 
+{
+  make dist-clean
+  bash ./configure \
+    --with-debug-level=fastdebug \
+    --with-native-debug-symbols=internal \
+    --with-target-bits=64 \
+    --with-jobs=32 \
+    --with-extra-ldflags=-lregions
+
+  intercept-build make
+  cd ../ 
+  compdb -p jdk17u067 list > compile_commands.json
+  mv compile_commands.json jdk17u067
+  cd - || exit
+}
+
+function clean_make()
+{
+  make clean
+  make
+}
+
+export_env_vars()
+{
+	local PROJECT_DIR="$(pwd)/../"
+
+	export JAVA_HOME="/usr/java/jdk-17.0.4.1"
+
+	### TeraHeap Allocator
+	export LIBRARY_PATH=${PROJECT_DIR}/allocator/lib/:$LIBRARY_PATH
+	export LD_LIBRARY_PATH=${PROJECT_DIR}/allocator/lib/:$LD_LIBRARY_PATH                                                                                           
+	export PATH=${PROJECT_DIR}/allocator/include/:$PATH
+	export C_INCLUDE_PATH=${PROJECT_DIR}/allocator/include/:$C_INCLUDE_PATH                                                                                         
+	export CPLUS_INCLUDE_PATH=${PROJECT_DIR}/allocator/include/:$CPLUS_INCLUDE_PATH
+}
+
+while getopts ":drcmh" opt
+do
+  case "${opt}" in
+    r)
+      export_env_vars
+      release
+      ;;
+    d)
+      export_env_vars
+      debug_symbols_on
+      ;;
+    c)
+      echo "Clean and make"
+      export_env_vars
+      clean_make
+      ;;
+    m)
+      echo "Make"
+      export_env_vars
+      make
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
